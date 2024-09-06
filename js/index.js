d78a2c472a810bc17977308ac51081018982aa54
Yilin <yilin.zh135@gmail.com>
2024-09-06 23:23:24 +0200

    Crear sección de reseñas

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const opinionsImg = document.querySelector(".opinions__userImg img");
+const opinionsText = document.querySelector(".opinions__userText p");
+
+let res;
+let index = 0; // Índice para recorrer los datos
+
+const request = new XMLHttpRequest();
+request.open("GET", "../json/review.json");
+request.responseType = "json";
+request.send();
+
+request.onload = function () {
+    res = request.response;
+    console.log(res); 
+
+    //Llamar a la función si el json no está vacío
+    if (res.length > 0) {
+        setInterval(updateContent, 10000);
+    }
+};
+
+//Función para cambiar el contenido cada 10s
+function updateContent() {
+    opinionsImg.src = res[index].img; 
+    opinionsText.textContent = res[index].review; 
+    // Incrementar el i y reiniciar cuando llegue al final
+    index = (index + 1) % res.length;
+    console.log(index);
+}
