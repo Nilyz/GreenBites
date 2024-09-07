47f83aa2b93652a40049a4c15ecd38a720c734d1
Yilin <yilin.zh135@gmail.com>
2024-09-07 16:09:50 +0200

    Contenido de la galería

---
+++ js/galeria.js
@@ -0,0 +1,78 @@
+
+const dishSection__cardCont = document.querySelector(".dishSection__cardCont");
+
+let res;
+
+
+const request = new XMLHttpRequest();
+request.open("GET", "../json/platos.json");
+request.responseType = "json";
+request.send();
+
+request.onload = function () {
+    res = request.response;
+    console.log(res); 
+
+    if (res && res.length > 0) {
+        createDishCards(res); // Llamar a la función pasando los datos del JSON
+    }
+};
+
+function createDishCards(platos){
+    platos.forEach(plato => {
+        const dishCont = document.createElement('div');
+                dishCont.classList.add('dishCont');
+
+                // Crear la imagenCont
+                const dish__imgCont = document.createElement('div');
+                dish__imgCont.classList.add('dish__imgCont'); 
+                
+                const dishImg = document.createElement('img');
+                dishImg.src = plato.dishImg; 
+                dishImg.alt = plato.dishName;
+
+                //  Div de info
+                const dish__infoCont = document.createElement("div");
+                dish__infoCont.classList.add('dish__infoCont');
+
+                const dish__infoCont__name = document.createElement("div");
+                dish__infoCont__name.classList.add('dish__infoCont__name');
+                const dishName = document.createElement('p');
+                dishName.textContent = plato.dishName;
+                
+
+                const dish__infoCont__time = document.createElement("div");
+                dish__infoCont__time.classList.add('dish__infoCont__time');
+                const timeImg = document.createElement('img');
+                timeImg.src = "../img/clock.svg"; 
+                timeImg.alt = "reloj";
+                const preparationTime = document.createElement('p');
+                preparationTime.textContent = plato.preparationTime;
+
+                const dish__price = document.createElement("div");
+                dish__price.classList.add('dish__price');
+                const price = document.createElement('p');
+                price.textContent = plato.price;
+                const orderLink = document.createElement('a');
+                orderLink.href = "#"; 
+                orderLink.textContent = "Ordenar Ahora";
+
+                // Añadir todo al contenedor
+                dishCont.appendChild(dish__imgCont);
+                dishCont.appendChild(dish__infoCont);
+                dish__imgCont.appendChild(dishImg);
+                dish__infoCont.appendChild(dish__infoCont__name);
+                dish__infoCont__name.appendChild(dishName);
+                dish__infoCont.appendChild(dish__infoCont__time);
+                dish__infoCont__time.appendChild(timeImg);
+                dish__infoCont__time.appendChild(preparationTime);
+                dish__infoCont.appendChild(dish__price);
+                dish__price.appendChild(price);
+                dish__price.appendChild(orderLink);
+                
+
+                // Añadir el contenedor al DOM
+                dishSection__cardCont.appendChild(dishCont);
+    })
+    
+}
